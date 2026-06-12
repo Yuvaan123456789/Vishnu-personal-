ed0c1bd20bb4696950967073e64df8e04b54b8e8
Yuvaan123456789 <patelvishnu9751@gmail.com>
2026-06-12 12:24:41 +0530

    new
    new

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 
 console.log("Insta clone running...");
+function like(btn) {
+    let likeText = btn.parentElement.querySelector(".likes");
+    let count = parseInt(likeText.innerText);
+
+    likeText.innerText = (count + 1) + " likes";
+}
